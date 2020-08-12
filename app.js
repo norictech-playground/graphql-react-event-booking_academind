4e1ac42cbe107f92bc038ecd8995acc9b39a35e7
norictech <ahmad.uji1902@gmail.com>
2020-08-12 21:25:04 +0700

    graphql type, query, mutation & schema

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import { buildSchema } from 'graphql'
+import { graphqlHTTP } from 'express-graphql'
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use(
+    '/gql',
+    graphqlHTTP({
+        graphiql: true,
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
+
+            type RootMutation {
+                createEvent(name: String!): String
+            }
+
+            schema {
+                query: RootQuery,
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => ['Romantic Cooking', 'All-night Coding'],
+            createEvent: ({ name }) => name
+        }
+    })
+)
 
 app.listen(3000, () => console.log(`App running on port 3000`))
