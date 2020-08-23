0a93bfa1da0d45be39aaff286147ffa3539e7fda
norictech <ahmad.uji1902@gmail.com>
2020-08-23 19:44:11 +0700

    refactor

--- app.js
+++ app.js
@@ -1,60 +1,54 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import { buildSchema } from 'graphql'
 import { graphqlHTTP } from 'express-graphql'
 
 const app = express()
+let events = []
 
-app.use(bodyParser.json())
+let schema = buildSchema(`
+    type Event {
+        _id: String!
+        title: String!
+        description: String!
+    }
 
-const events = []
-app.use(
-    '/gql',
-    graphqlHTTP({
-        graphiql: true,
-        schema: buildSchema(`
-            type Event {
-                _id: String!
-                title: String!
-                description: String!
-                date: String!
-            }
+    type Query {
+        events: [Event!]!
+    }
 
-            type RootQuery {
-                events: [Event!]!
-            }
+    input EventInput {
+        title: String!
+        description: String!
+    }
 
-            input EventInput {
-                _id: String!
-                title: String!
-                description: String!
-                date: String!
-            }
+    type Mutation {
+        createEvent(input: EventInput): Event
+    }
 
-            type RootMutation {
-                createEvent(input: EventInput): Event
-            }
+    schema {
+        query: Query,
+        mutation: Mutation
+    }
+`)
 
-            schema {
-                query: RootQuery,
-                mutation: RootMutation
-            }
-        `),
-        rootValue: {
-            events: () => events,
-            createEvent: ({ input }) => {
-                const event = {
-                    _id: Math.random(),
-                    title: input.title,
-                    description: input.description,
-                    date: input.date
-                }
-                events.push(event)
-                
-                return event
-            }
+let root = {
+    events: () => events,
+    createEvent: ({ input }) => {
+        const createdEvent = {
+            _id: Math.random().toString(),
+            title: input.title,
+            description: input.description
         }
-    })
-)
+        events.push(createdEvent)
+
+        return createdEvent
+    }
+}
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+    rootValue: root
+}))
 
 app.listen(3000, () => console.log(`App running on port 3000`))
